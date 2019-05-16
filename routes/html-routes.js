25fe9cab8275fd9135dbc4f1e381ef244e5a866f
mattwills09 <mattwills09@gmail.com>
2019-05-16 14:47:39 -0400

    fixing login route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -38,12 +38,10 @@ module.exports = function(app) {
   });
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
-
   // index route loads view.html
   // app.get("/", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/blog.html"));
   // });
-
 
   // cms route loads cms.html
   app.get("/cms", function(req, res) {
@@ -65,8 +63,6 @@ module.exports = function(app) {
 //     });
 //   });
 // });
-
-
 
 // //===============================
 // //Load One Event by ID
