519da93b34c586cd7db6bac26174db0f1cdabf1c
mattwills09 <mattwills09@gmail.com>
2019-05-16 14:44:44 -0400

    updated routes html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -58,41 +58,41 @@ module.exports = function(app) {
 
 //===============================
 //Load ALL Events Page
-app.get("/events", function(req, res) {
-  db.Event.findAll({}).then(function(results) {
-    res.render("events", {
-      Event: results
-    });
-  });
-});
+// app.get("/events", function(req, res) {
+//   db.Event.findAll({}).then(function(results) {
+//     res.render("events", {
+//       Event: results
+//     });
+//   });
+// });
 
 
 
-//===============================
-//Load One Event by ID
-app.get("/events/:id", function(req, res) {
-  db.Event.findOne({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(results) {
-    res.render("oneEvent", {
-    Event: results
-    });
-  });
-});
+// //===============================
+// //Load One Event by ID
+// app.get("/events/:id", function(req, res) {
+//   db.Event.findOne({
+//     where: {
+//       id: req.params.id
+//     }
+//   }).then(function(results) {
+//     res.render("oneEvent", {
+//     Event: results
+//     });
+//   });
+// });
 
-//===============================
-//Add New Event
-app.get("/newevent", function(req,res) {
-  db.Event.findAll({
-    limit: 3
-  }).then(function(results) {
-    res.render("add", {
-      Event: results
-    });
-});
-});
+// //===============================
+// //Add New Event
+// app.get("/newevent", function(req,res) {
+//   db.Event.findAll({
+//     limit: 3
+//   }).then(function(results) {
+//     res.render("add", {
+//       Event: results
+//     });
+// });
+// });
 
 //===============================
 // Render 404 page for any unmatched routes
